c2f68591e19225c9b7cc9ad1cd6de92bdcc905f5
Anton Zabolotnikob <azabolotnikov@ronasit.com>
2025-08-22 12:08:57 +0500

    chore: fix code style

--- tests/ChannelTest.php
+++ tests/ChannelTest.php
@@ -2,6 +2,10 @@
 
 namespace NotificationChannels\ExpoPushNotifications\Test;
 
+use ExponentPhpSDK\Expo;
+use Illuminate\Events\Dispatcher;
+use Mockery;
+use NotificationChannels\ExpoPushNotifications\ExpoChannel;
 use NotificationChannels\ExpoPushNotifications\Test\Support\ChannelMockTrait;
 use NotificationChannels\ExpoPushNotifications\Test\Support\Notifications\TestNotifiable;
 use NotificationChannels\ExpoPushNotifications\Test\Support\Notifications\TestNotification;
@@ -11,28 +15,33 @@ class ChannelTest extends TestCase
     use ChannelMockTrait;
 
     protected TestNotifiable $notifiable;
+    protected Expo $expoMock;
+    protected Dispatcher $events;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->notification = new TestNotification;
+        $this->notification = new TestNotification();
+        $this->expoMock = Mockery::mock(Expo::class);
+        $this->events = Mockery::mock(Dispatcher::class);
     }
 
     public function testItCanSendANotification()
     {
-        $this->notification->toExpoPush(new TestNotifiable);
+        $this->notification->toExpoPush(new TestNotifiable());
 
-        $this->assertSendNotificationMessage('channel_message.json');
+        $this->assertNotificationSend('channel_message.json');
+
+        (new ExpoChannel($this->expoMock, $this->events))->send(new TestNotifiable(), new TestNotification());
     }
 
     public function testItFiresFailureEventOnFailure()
     {
-        $this->notification->toExpoPush(new TestNotifiable);
+        $this->notification->toExpoPush(new TestNotifiable());
 
-        $this->assertSendNotificationMessage(
-            expectedFixture: 'channel_message.json',
-            throwException: true,
-        );
+        $this->assertNotificationNotSend('channel_message.json');
+
+        (new ExpoChannel($this->expoMock, $this->events))->send(new TestNotifiable(), new TestNotification());
     }
 }
--- tests/Support/ChannelMockTrait.php
+++ tests/Support/ChannelMockTrait.php
@@ -3,40 +3,32 @@
 namespace NotificationChannels\ExpoPushNotifications\Test\Support;
 
 use ExponentPhpSDK\Exceptions\ExpoException;
-use ExponentPhpSDK\Expo;
 use Illuminate\Notifications\Events\NotificationFailed;
 use Mockery;
-use NotificationChannels\ExpoPushNotifications\ExpoChannel;
-use NotificationChannels\ExpoPushNotifications\Test\Support\Notifications\TestNotifiable;
-use NotificationChannels\ExpoPushNotifications\Test\Support\Notifications\TestNotification;
-use Illuminate\Events\Dispatcher;
 
 trait ChannelMockTrait
 {
-    protected function assertSendNotificationMessage(string $expectedFixture, bool $throwException = false): void
+    protected function assertNotificationSend(string $expectedFixture): void
     {
-        $expectedData = $this->getJsonFixture($expectedFixture);
-
-        $expoMock = Mockery::mock(Expo::class);
-        $events = Mockery::mock(Dispatcher::class);
-
-        $expo = $expoMock
+        $this->expoMock
             ->shouldReceive('notify')
             ->once()
-            ->with(['interest_name'], $expectedData, true);
+            ->with(['interest_name'], $this->getJsonFixture($expectedFixture), true)
+            ->andReturn(['status' => 'ok']);
+    }
 
-        if ($throwException) {
-            $expo->andThrow(ExpoException::class);
+    protected function assertNotificationNotSend(string $expectedFixture): void
+    {
+        $this->expoMock
+            ->shouldReceive('notify')
+            ->once()
+            ->with(['interest_name'], $this->getJsonFixture($expectedFixture), true)
+            ->andThrow(ExpoException::class);
 
-            $events
-                ->shouldReceive('dispatch')
-                ->once()
-                ->with(Mockery::type(NotificationFailed::class));
-        }
-        else {
-            $expo->andReturn(['status' => 'ok']);
-        }
+        $this->events
+            ->shouldReceive('dispatch')
+            ->once()
+            ->with(Mockery::type(NotificationFailed::class));
 
-        (new ExpoChannel($expoMock, $events))->send(new TestNotifiable, new TestNotification);
     }
 }
