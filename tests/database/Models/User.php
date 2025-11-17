6ab4c7884631a90760743dbca790fb6ea7da3005
DenTray <dpankratov@ronasit.com>
2025-11-17 11:41:40 +0600

    Update tests/database/Models/User.php

--- tests/database/Models/User.php
+++ tests/database/Models/User.php
@@ -9,13 +9,4 @@ use RonasIT\Support\Traits\ModelTrait;
 
 class User extends Authenticatable
 {
-    protected $table = 'users';
-
-    use Notifiable;
-    use ModelTrait;
-    use HasFactory;
-
-    protected $fillable = [
-        'email',
-    ];
 }
