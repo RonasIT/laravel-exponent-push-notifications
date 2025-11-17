cae943e90aae879609d88fe92632c6a872499dc6
DenTray <dpankratov@ronasit.com>
2025-11-17 11:41:57 +0600

    Update tests/database/Models/User.php

--- tests/database/Models/User.php
+++ tests/database/Models/User.php
@@ -2,10 +2,7 @@
 
 namespace NotificationChannels\ExpoPushNotifications\Test\database\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use RonasIT\Support\Traits\ModelTrait;
 
 class User extends Authenticatable
 {
